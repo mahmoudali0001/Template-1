ee6c8e8a48a2dc18d2a9eeac99087f535bdde736
Mahmoud Ali <mahmoudalifarag1@gmail.com>
2022-04-12 17:55:57 +0200

    Add All Section's

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 let headerIcon = document.querySelector(".header .links");
 let icon = document.querySelector(".header .links ul");
+let up = document.querySelector(".up");
 
 headerIcon.onclick = function () {
   if (icon.style.display != "block") {
@@ -7,4 +8,15 @@ headerIcon.onclick = function () {
   } else {
     icon.style.display = "none";
   }
+};
+
+window.onscroll = function () {
+  this.scrollY >= 1000 ? up.classList.add("show") : up.classList.remove("show");
+};
+
+up.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 };
